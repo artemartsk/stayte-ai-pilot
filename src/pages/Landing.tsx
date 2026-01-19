98a53a7e18a97c3598d2a038f74e66c084e04b71
artemartsokolov <as@reviero.com>
2026-01-19 14:43:12 +0300

    Update Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -98,7 +98,7 @@ export function Landing() {
 
                                     <h1
                                         className="mt-8 max-w-4xl mx-auto text-balance text-6xl md:text-7xl lg:mt-16 xl:text-[5.25rem] font-bold tracking-tight">
-                                        Modern Solutions for Real Estate
+                                        AI Copilot for Real Estate Agencies
                                     </h1>
                                     <p
                                         className="mx-auto mt-8 max-w-2xl text-balance text-lg text-muted-foreground">
